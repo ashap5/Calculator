78e94670545fbc63a568018dcd0447ba80d96f3c
Pasha 5 <pashaust2@gmail.com>
2021-06-24 23:45:23 +0200

    Test

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <string>
-#include "Calculator.h"
 using namespace std;
-
-
-int main()
-{
-    PrintString("");
-    return 0;
-}
 
 void PrintString(string text)
 {
-    cout << "test";
+    cout << text;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+float Calculator(float firstNumber, float SecondNumber, string Operation)
+{
+    if (Operation == "+")
+    {
+        return firstNumber + SecondNumber;
+    }
+    else if (Operation == "-")
+    {
+        return firstNumber - SecondNumber;
+    }
+    else if (Operation == "*")
+    {
+        return firstNumber * SecondNumber;
+    }
+    else if (Operation == "/")
+    {
+        return firstNumber / SecondNumber;
+    }
+    else return 0;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+    cout << Calculator(1, 5, "*");
+    PrintString("\nCAUTE MORE");
+    return 0;
+}
